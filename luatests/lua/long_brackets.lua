becfc9a27a55e53532808b90ac707c78376d4686
Arnaud Delobelle <arnodel@gmail.com>
2018-05-15 21:35:22 +0100

    Long brackets luat tests

---
+++ luatests/lua/long_brackets.lua
@@ -0,0 +1,24 @@
+print([[hello world!]])
+--> =hello world!
+
+print([==[A long ]] ]=] ]===] bracket]==])
+--> =A long ]] ]=] ]===] bracket
+
+print([===[a
+b
+c]===])
+--> =a
+--> =b
+--> =c
+
+print([[
+xxx]])
+--> =xxx
+
+--[[
+A long comment
+]]
+
+--[====[
+Another [==[ long ]==] comment
+]====]
