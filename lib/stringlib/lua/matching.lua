30d4cc60cbbf0c7c1c9a498f711df5a6d52c6912
Arnaud Delobelle <arnodel@gmail.com>
2018-11-24 11:51:47 +0000

    Better coverage for stringlib matching functions

---
+++ lib/stringlib/lua/matching.lua
@@ -0,0 +1,206 @@
+local function errtest(f)
+    return function(...)
+        local ok, err = pcall(f, ...)
+        if ok then
+            print"OK"
+        else
+            print(err)
+        end
+    end
+end
+
+do
+    local function pf(...)
+        print(string.find(...))
+    end
+    
+    pf("a", "a", 1, true)
+    --> =1	1
+
+    pf("hello world!", "o w", 1, true)
+    --> =5	7
+
+    pf("xyzt", "yt", 1, true)
+    --> =nil
+
+    pf("1234 abc453", "%l+")
+    --> =6	8
+
+    pf("  foo=[a [lovely] day];", "(%w+)=(%b[])")
+    --> =3	22	foo	[a [lovely] day]
+
+    print(pcall(string.find, "abc", "(xx%1)"))
+    --> ~false\t.*
+
+    local errf = errtest(string.find)
+
+    errf(1)
+    --> ~2 arguments needed
+
+    errf("x", "y", "z")
+    --> ~must be an integer
+
+    pf("x", "x", 3)
+    --> =nil
+
+    pf("x", "x", -10)
+    --> =nil
+
+    pf("x", "y")
+    --> =nil
+end
+
+do
+    local function pm(...)
+        print(string.match(...))
+    end
+
+    pm("Let me *stress* that I *am*", "%*.-%*")
+    --> =*stress*
+
+    pm("Let me *stress* that I *am*", "%*.-%*", 17)
+    --> =*am*
+
+    pm("Let me *stress* that I *am*", "%*(.-)%*")
+    --> =stress
+
+    pm("Let me *stress* that I *am*", "%*(.-)%*", 17)
+    --> =am
+
+    pm("A *bold* and an _underline_", "([*~_])(.-)%1")
+    --> =*	bold
+
+    pm("A *b_o_l_d* and an _under~line_", "([*~_])(.-)%1")
+    --> =*	b_o_l_d
+
+    pm("A *b_o_l_d* and an _under~line_", "([*~_])(.-)%1", 10)
+    --> =_	under~line
+
+    local errm = errtest(string.match)
+
+    errm("x")
+    --> ~2 arguments needed
+
+    errm("x", "%")
+    --> ~malformed pattern
+end
+
+do
+    local s = "hello world from Lua"
+    for w in string.gmatch(s, "%a+") do
+        print(w)
+    end
+    --> =hello
+    --> =world
+    --> =from
+    --> =Lua
+
+    local t = {}
+    local s = "from=world, to=Lua"
+    for k, v in string.gmatch(s, "(%w+)=(%w+)") do
+        t[k] = v
+    end
+    print(t.from, t.to)
+    --> =world	Lua
+
+    local errgm = errtest(string.gmatch)
+
+    errgm()
+    --> ~2 arguments needed
+
+    errgm("x")
+    --> ~2 arguments needed
+
+    errgm("x", "%")
+    --> ~malformed pattern
+
+    for w in string.gmatch("abc", "b*") do
+        print(w)
+    end
+    --> =
+    --> =b
+    --> =
+end
+
+do
+    local function pgs(...)
+        print(string.gsub(...))
+    end
+
+    pgs("hello world", "(%w+)", "%1 %1")
+    --> =hello hello world world	2
+
+    pgs("hello world", "%w+", "%0 %0", 1)
+    --> =hello hello world	1
+
+    pgs("hello world from Lua", "(%w+)%s*(%w+)", "%2 %1")
+    --> =world hello Lua from	2
+
+    local function getenv(v)
+        if v == "HOME" then
+            return "/home/roberto"
+        elseif v == "USER" then
+            return "roberto"
+        end
+    end
+
+    pgs("home = $HOME, user = $USER", "%$(%w+)", getenv)
+    --> =home = /home/roberto, user = roberto	2
+
+    pgs("4+5 = $return 4+5$", "%$(.-)%$",
+                      function (s)
+                          return load(s)()
+                      end
+    )
+    --> =4+5 = 9	1
+
+    local t = {name="lua", version="5.3"}
+    pgs("$name-$version.tar.gz", "%$(%w+)", t)
+    --> =lua-5.3.tar.gz	2
+
+    local errgs = errtest(string.gsub)
+
+    errgs()
+    --> ~3 arguments needed
+
+    errgs("x", "y")
+    --> ~3 arguments needed
+
+    errgs("x", "y", "z", "t")
+    --> ~must be an integer
+
+    errgs("x", "%", "z")
+    --> ~malformed pattern
+
+    pgs("xyz", "()y()", "%1-%2")
+    --> =x2-3z	1
+
+    errgs("xyz", "(x)", "%2")
+    --> ~invalid capture index
+
+    pgs("xyz", "xyz", "%%")
+    --> =%	1
+
+    errgs("xyz", "xyz", "%x %%")
+    --> ~invalid.*%
+
+    local replt = {}
+    setmetatable(replt, {__index=function() error("boo") end})
+    errgs("xyz", "xyz", replt)
+    --> =boo
+
+    errgs("xyz", "xyz", function () error("baa") end)
+    --> =baa
+
+    errgs("xyz", "xyz", false)
+    --> ~must be a string, table or function
+    
+    errgs("z", "z", {z=true})
+    --> ~invalid replacement
+
+    pgs("z", "z", {z=false})
+    --> =z	1
+
+    pgs("abc", "b*", "Z")
+    --> =ZaZcZ	4
+end
---
+++ lib/stringlib/lua/packing.lua
@@ -0,0 +1,199 @@
+do
+    local function pack(...)
+        print(string.byte(string.pack(...), 1, -1))
+    end
+    
+    pack("bB", 100, 200)
+    --> =100	200
+
+    pack("<i", 1)
+    --> =1	0	0	0
+
+    pack(">i", 1)
+    --> =0	0	0	1
+
+    pack("!2bh", 10, 20)
+    --> =10	0	20	0
+
+    pack("<!4zs", "A", "BCD")
+    --> =65	0	0	0	3	0	0	0	0	0	0	0	66	67	68
+
+    pack("<i6", 123456789)
+    --> =21	205	91	7	0	0
+
+    pack(">i6", 123456789)
+    --> =0	0	7	91	205	21
+
+    pack(">i10", 9876543210)
+    --> =0	0	0	0	0	2	76	176	22	234
+
+    pack("<i10", 9876543210)
+    --> =234	22	176	76	2	0	0	0	0	0
+
+    pack("xx")
+    --> =0	0
+
+    pack("x!4Xjz", "A")
+    --> =0	0	0	0	65	0
+
+    pack("Hc4", 65535, "AB")
+    --> =255	255	65	66	0	0
+
+    pack("<d", 123.456)
+    --> =119	190	159	26	47	221	94	64
+
+    pack("<f", 1e-3)
+    --> =111	18	131	58
+
+    pack("<J", 1000)
+    --> =232	3	0	0	0	0	0	0
+
+    pack(">I10", 9876543210)
+    --> =0	0	0	0	0	2	76	176	22	234
+
+    pack("<I10", 9876543210)
+    --> =234	22	176	76	2	0	0	0	0	0
+
+    pack("<I6", 123456789)
+    --> =21	205	91	7	0	0
+
+    pack(">I6", 123456789)
+    --> =0	0	7	91	205	21
+
+    local function packError(...)
+        if pcall(string.pack, ...) then
+            print("NO ERROR")
+        end
+    end
+
+    packError("d") -- missing value
+    packError("i", "abc") -- bad value type
+    packError("!17") -- size out of bounds
+    packError("b", 128) -- value out of bounds
+    packError("B", -1) -- value out of bounds
+    packError("cxx", "abc") -- missing string length
+    packError("c2", "abc") -- string too long
+    packError("dy", 1) -- invalid option "y"
+    packError("bbX", 1, 1) -- "X" must be followed by option
+    packError("!3bi", 1, 1) -- alignment not a power of 2
+end
+
+do
+    local function unpack(...)
+        print(string.unpack(...))
+    end
+
+    unpack("b", "A")
+    --> =65	2
+
+    unpack("<i", "abcd")
+    --> =1684234849	5
+
+    unpack(">i", "abcd")
+    --> =1633837924	5
+
+    unpack("<bc2H", "Bhi\x00\x04")
+    --> =66	hi	1024	6
+
+    unpack(">s1xB", "\x05hello*\x80")
+    --> =hello	128	9
+
+    unpack("B", "1234\xff678", 5)
+    --> =255	6
+
+    unpack("<!4zi", "hi\x00*\x00\x00\x01\x00")
+    --> =hi	65536	9
+
+    unpack("!4c1Xlc1", "A***B")
+    --> =A	B	6
+
+    unpack("<hh", "\x01\x00\x00\x01")
+    --> =1	256	5
+
+    unpack("<J", "\x00\x00\x01\x00\x00\x00\x00\x00")
+    --> =65536	9
+
+    unpack(">I", "\xff\xff\xff\xff")
+    --> =4294967295	5
+
+    unpack("<f", "\x00\x00\x00\x00")
+    --> =0	5
+
+    unpack("<d", "\x00\x00\x00\x00\x00\x00\x00\x00")
+    --> =0	9
+
+    unpack("<I8", "\x00\x00\x01\x00\x00\x00\x00\x00")
+    --> =65536	9
+
+    unpack("<I9", "\x00\x00\x01\x00\x00\x00\x00\x00\x00")
+    --> =65536	10
+
+    unpack("<i8", "\x00\x00\x01\x00\x00\x00\x00\x00")
+    --> =65536	9
+
+    unpack("<i9", "\x00\x00\x01\x00\x00\x00\x00\x00\x00")
+    --> =65536	10
+
+    unpack("<i10", "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff")
+    --> =-1	11
+
+    unpack(">i10", "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff")
+    --> =-1	11
+
+    unpack("<i6", "\xff\xff\x00\x00\x00\x00")
+    --> =65535	7
+
+    unpack(">i6", "\x00\x00\x00\x00\xff\xff")
+    --> =65535	7
+
+    unpack("<i3>i3", "\xff\xff\xff\xff\xff\xff")
+    --> =-1	-1	7
+
+    local function unpackError(...)
+        if pcall(string.unpack, ...) then
+            print("NO ERROR")
+        end
+    end
+
+    unpackError("b", "")
+    unpackError("i", "123")
+    unpackError("bX", "2")
+    unpackError("z", "abc")
+    unpackError("c5", "abcd")
+    unpackError("By", "a")
+    unpackError("s1", "\x3ab")
+    unpackError("XX", "hello")
+    unpackError("X ", "hello")
+end
+
+do
+    local function ps(f)
+        print(string.packsize(f))
+    end
+
+    ps("bb")
+    --> =2
+
+    ps("c10i")
+    --> =14
+
+    ps("lx")
+    --> =9
+
+    ps("!4Bf")
+    --> =8
+
+    ps("!8hXi8")
+    --> =8
+
+    local function psError(f)
+        if pcall(string.packsize, f) then
+            print("NO ERROR")
+        end
+    end
+
+    psError("c")
+    psError("!20")
+    psError("z")
+    psError("s4")
+end
