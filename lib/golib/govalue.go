5079c4f106f3db76554e81083510497b9926de54
Arnaud Delobelle <arnodel@gmail.com>
2019-07-22 21:21:05 +0100

    Improve error messages in lua/go conversions

--- lib/golib/govalue.go
+++ lib/golib/govalue.go
@@ -2,6 +2,7 @@ package golib
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 
 	rt "github.com/arnodel/golua/runtime"
@@ -44,11 +45,11 @@ func (g GoValue) Index(v rt.Value, meta *rt.Table) (rt.Value, error) {
 		if f != (reflect.Value{}) {
 			return reflectToValue(f, meta), nil
 		}
-		return nil, errors.New("could not find field or method with name: " + string(field))
+		return nil, fmt.Errorf("no field or method with name %q", field)
 	case reflect.Map:
-		goV := valueToType(v, gv.Type().Key())
-		if goV == (reflect.Value{}) {
-			return nil, errors.New("map index or incorrect type")
+		goV, err := valueToType(v, gv.Type().Key())
+		if err != nil {
+			return nil, fmt.Errorf("map index or incorrect type: %s", err)
 		}
 		return reflectToValue(gv.MapIndex(goV), meta), nil
 	case reflect.Slice:
@@ -84,20 +85,20 @@ func (g GoValue) SetIndex(key rt.Value, val rt.Value) error {
 		if !f.CanSet() {
 			return errors.New("struct field cannot be set")
 		}
-		goVal := valueToType(val, f.Type())
-		if goVal == (reflect.Value{}) {
-			return errors.New("struct field set to incompatible type")
+		goVal, err := valueToType(val, f.Type())
+		if err != nil {
+			return fmt.Errorf("struct field of incompatible type: %s", err)
 		}
 		f.Set(goVal)
 		return nil
 	case reflect.Map:
-		goKey := valueToType(key, gv.Type().Key())
-		if goKey == (reflect.Value{}) {
-			return errors.New("map key of incompatible type")
+		goKey, err := valueToType(key, gv.Type().Key())
+		if err != nil {
+			return fmt.Errorf("map key of incompatible type: %s", err)
 		}
-		goVal := valueToType(val, gv.Type().Elem())
-		if goVal == (reflect.Value{}) {
-			return errors.New("map value set to incompatible type")
+		goVal, err := valueToType(val, gv.Type().Elem())
+		if err != nil {
+			return fmt.Errorf("map value set to incompatible type: %s", err)
 		}
 		gv.SetMapIndex(goKey, goVal)
 		return nil
@@ -106,9 +107,9 @@ func (g GoValue) SetIndex(key rt.Value, val rt.Value) error {
 		if !ok {
 			return errors.New("slice idnex must be an integer")
 		}
-		goVal := valueToType(val, gv.Type().Elem())
-		if goVal == (reflect.Value{}) {
-			return errors.New("slice item set to incompatible type")
+		goVal, err := valueToType(val, gv.Type().Elem())
+		if err != nil {
+			return fmt.Errorf("slice item set to incompatible type: %s", err)
 		}
 		gv.Index(int(i)).Set(goVal)
 		return nil
@@ -129,11 +130,12 @@ func (g GoValue) Call(args []rt.Value, meta *rt.Table) ([]rt.Value, error) {
 		numParams--
 	}
 	var goArg reflect.Value
+	var err error
 	for i := 0; i < numParams; i++ {
 		if i < len(args) {
-			goArg = valueToType(args[i], f.In(i))
-			if goArg == (reflect.Value{}) {
-				return nil, errors.New("argument of incorrect type")
+			goArg, err = valueToType(args[i], f.In(i))
+			if err != nil {
+				return nil, err
 			}
 		} else {
 			goArg = reflect.Zero(f.In(i))
@@ -147,9 +149,9 @@ func (g GoValue) Call(args []rt.Value, meta *rt.Table) ([]rt.Value, error) {
 		etcLen := len(args) - numParams
 		etc := reflect.MakeSlice(etcSliceType, etcLen, etcLen)
 		for i := 0; i < etcLen; i++ {
-			goArg = valueToType(args[i+numParams], etcType)
-			if goArg == (reflect.Value{}) {
-				return nil, errors.New("argument of incorrect type")
+			goArg, err = valueToType(args[i+numParams], etcType)
+			if err != nil {
+				return nil, err
 			}
 			etc.Index(i).Set(goArg)
 		}
@@ -165,11 +167,11 @@ func (g GoValue) Call(args []rt.Value, meta *rt.Table) ([]rt.Value, error) {
 	return res, nil
 }
 
-func fillStruct(s reflect.Value, v rt.Value) bool {
+func fillStruct(s reflect.Value, v rt.Value) error {
 	var ok bool
 	t, ok := v.(*rt.Table)
 	if !ok {
-		return false
+		return errors.New("fillStruct: can only fill from a table")
 	}
 	var fk, fv rt.Value
 	for {
@@ -179,46 +181,49 @@ func fillStruct(s reflect.Value, v rt.Value) bool {
 		}
 		name, ok := fk.(rt.String)
 		if !ok {
-			return false
+			return errors.New("fillStruct: table fields must be strings")
 		}
 		field := s.FieldByName(string(name))
 		if field == (reflect.Value{}) {
-			return false
+			return fmt.Errorf("fillStruct: field %q does not exist in struct", name)
 		}
-		goFv := valueToType(fv, field.Type())
-		if goFv == (reflect.Value{}) {
-			return false
+		goFv, err := valueToType(fv, field.Type())
+		if err != nil {
+			return err
 		}
 		field.Set(goFv)
 	}
-	return true
+	return nil
 }
 
-func valueToType(v rt.Value, tp reflect.Type) reflect.Value {
+func valueToType(v rt.Value, tp reflect.Type) (reflect.Value, error) {
 	if goVal, _, ok := ValueToGoValue(v); ok {
 		if goVal.value.Type().AssignableTo(tp) {
-			return goVal.value
+			return goVal.value, nil
 		}
-		return reflect.Value{}
+		if goVal.value.Type().ConvertibleTo(tp) {
+			return goVal.value.Convert(tp), nil
+		}
+		return reflect.Value{}, fmt.Errorf("%+v is not assignable or convertible to %s", goVal.value.Interface(), tp.Name())
 	}
 	var goV interface{}
 	var ok bool
 	switch tp.Kind() {
 	case reflect.Ptr:
 		if tp.Elem().Kind() != reflect.Struct {
-			return reflect.Value{}
+			return reflect.Value{}, fmt.Errorf("lua value cannot be converted to %s", tp.Name())
 		}
 		p := reflect.New(tp.Elem())
-		if !fillStruct(p.Elem(), v) {
-			return reflect.Value{}
+		if err := fillStruct(p.Elem(), v); err != nil {
+			return reflect.Value{}, err
 		}
-		return p
+		return p, nil
 	case reflect.Struct:
 		s := reflect.Zero(tp)
-		if !fillStruct(s, v) {
-			return reflect.Value{}
+		if err := fillStruct(s, v); err != nil {
+			return reflect.Value{}, err
 		}
-		return s
+		return s, nil
 	case reflect.Int:
 		var x rt.Int
 		x, ok = rt.ToInt(v)
@@ -241,9 +246,9 @@ func valueToType(v rt.Value, tp reflect.Type) reflect.Value {
 		}
 	}
 	if !ok {
-		return reflect.Value{}
+		return reflect.Value{}, fmt.Errorf("%+v cannot be converted to %s", v, tp.Name())
 	}
-	return reflect.ValueOf(goV)
+	return reflect.ValueOf(goV), nil
 }
 
 func reflectToValue(v reflect.Value, meta *rt.Table) rt.Value {
