4e32facb3d0501ac02f208b0f78665c878ab8122
Arnaud Delobelle <arnodel@gmail.com>
2020-12-07 20:08:17 +0000

    Some doc for the pattern package

--- lib/stringlib/pattern/pattern.go
+++ lib/stringlib/pattern/pattern.go
@@ -2,17 +2,23 @@ package pattern
 
 import "errors"
 
+// A Pattern is a data structure able to interpret a Lua "pattern" (see e.g.
+// https://www.lua.org/manual/5.3/manual.html#6.4.1).
 type Pattern struct {
 	items                  []patternItem
 	captureCount           int
 	startAnchor, endAnchor bool
 }
 
+// New returns a new Pattern built from the given string (or an error if it is
+// not a valid pattern string).
 func New(ptn string) (*Pattern, error) {
 	pb := &patternBuilder{ptn: ptn}
 	return pb.getPattern()
 }
 
+// MatchFromStart returns a slice of Capture instances that match the given
+// string, starting from the `init` index.
 func (p *Pattern) MatchFromStart(s string, init int) []Capture {
 	matcher := patternMatcher{
 		Pattern: *p,
@@ -22,6 +28,8 @@ func (p *Pattern) MatchFromStart(s string, init int) []Capture {
 	return matcher.findFromStart()
 }
 
+// Match returns a slice of Capture instances that match the given
+// string, starting from the `init` index.
 func (p *Pattern) Match(s string, init int) []Capture {
 	matcher := patternMatcher{
 		Pattern: *p,
@@ -31,18 +39,22 @@ func (p *Pattern) Match(s string, init int) []Capture {
 	return matcher.find()
 }
 
+// A Capture represents a matching substring.
 type Capture struct {
 	start, end int
 }
 
+// Start index of the capture.
 func (c Capture) Start() int {
 	return c.start
 }
 
+// End index of the capture.
 func (c Capture) End() int {
 	return c.end
 }
 
+// IsEmpty is trye if the Capture is empty.
 func (c Capture) IsEmpty() bool {
 	return c.end == -1
 }
