766a6bb171426dee3e16da0597501093b6a8d295
Arnaud Delobelle <arnodel@gmail.com>
2021-08-16 17:28:03 +0100

    Documentation and better naming for hashtable implementation

--- runtime/hashtable.go
+++ runtime/hashtable.go
@@ -5,18 +5,25 @@ import (
 	"unsafe"
 )
 
+// Number of bits in an uintptr
 const uintptrLen = 8 * unsafe.Sizeof(uintptr(0))
 
 //
 // Implementation for Lua table.  It is made of two parts, a hash table and an
 // array, the latter containing only values with positive integer keys.
 //
+// The Value type needs to satisfy the interface {
+//     Hash() uintptr
+//     IsNil() bool
+//     Equals(Value) bool
+// }
 
 type mixedTable struct {
 	*hashTable
 	*array
 }
 
+// Return v such that k => v, else return nil.
 func (t *mixedTable) get(k Value) Value {
 	i, ok := ToIntNoString(k)
 	if ok {
@@ -28,6 +35,7 @@ func (t *mixedTable) get(k Value) Value {
 	return t.hashTable.find(k)
 }
 
+// Set k => v
 func (t *mixedTable) insert(k, v Value) {
 	i, ok := ToIntNoString(k)
 	if ok && t.array.setValue(i, v) {
@@ -45,6 +53,8 @@ func (t *mixedTable) insert(k, v Value) {
 	t.hashTable.set(k, v)
 }
 
+// Set k => v only if there is already v1 such that k => v1.  Returns true if
+// that is the case.
 func (t *mixedTable) reset(k, v Value) (wasSet bool) {
 	i, ok := ToIntNoString(k)
 	if ok {
@@ -59,6 +69,7 @@ func (t *mixedTable) reset(k, v Value) (wasSet bool) {
 	return t.hashTable.reset(k, v)
 }
 
+// Set k => nil, return true if there was v such that k => v.
 func (t *mixedTable) remove(k Value) (wasSet bool) {
 	i, ok := ToIntNoString(k)
 	if ok {
@@ -71,6 +82,8 @@ func (t *mixedTable) remove(k Value) (wasSet bool) {
 	return t.hashTable.removeKey(k)
 }
 
+// Return the "length" of the table, which is a positive integer such i => v but
+// (i + 1) => nil.
 func (t *mixedTable) len() uintptr {
 	l := t.array.getLen()
 	if l < t.array.size() {
@@ -82,6 +95,19 @@ func (t *mixedTable) len() uintptr {
 	return l
 }
 
+// Return the next key-value after k in the table if it exists (in which case ok
+// is true).  If k is nil, return the first key-value in the table.
+//
+// Provided no new key is inserted between successive calls of next(), then the
+// following code will iterate through all the key-value pairs in the table.
+//
+//    var k Value
+//    for {
+//        k, v, ok = t.next(k)
+//        if !ok {
+//            break
+//        }
+//    }
 func (t *mixedTable) next(k Value) (next Value, v Value, ok bool) {
 	var i int64
 	var isInt bool
@@ -109,6 +135,18 @@ func (t *mixedTable) next(k Value) (next Value, v Value, ok bool) {
 	return t.hashTable.next(k)
 }
 
+// Grow the table - either the hash table part or the array part.
+//
+// The array part grows if there is a power of 2, N, bigger than the current
+// size of the array and such that at least N/2 integer keys between 1 and N
+// belong to the table, including one >= N/2.  It then grows to size N and all
+// the keys between 1 and N are transferred to it.
+//
+// If the array part doesn't grow, then the hash table part grows by a factor of
+// 2.
+//
+// After growing the array it is guaranteed that there is at least one free slot
+// in the hash table part.
 func (t *mixedTable) grow() {
 	var idxCountByLen [uintptrLen]uintptr
 
@@ -133,8 +171,8 @@ func (t *mixedTable) grow() {
 
 	// Grow the array.  That should free capacity in the hashtable
 	array := t.array.grow(arrSize)
-	for i := range t.hashTable.items {
-		it := &t.hashTable.items[i]
+	for i := range t.hashTable.slots {
+		it := &t.hashTable.slots[i]
 		if it.value.IsNil() {
 			continue
 		}
@@ -151,54 +189,93 @@ func (t *mixedTable) grow() {
 // Hash table implementation
 //
 
-const smallHashTableSize = 8
-
-const (
-	hasNextFlag uintptr = 1
-	chainedFlag uintptr = 2
-	nextFlags           = hasNextFlag | chainedFlag
-)
-
-type hashTableItem struct {
-	key, value Value
-	next       uintptr
-}
-
-func (it hashTableItem) hasNext() bool {
-	return it.next&hasNextFlag != 0
-}
-
-func (it hashTableItem) nextIndex() uintptr {
-	return it.next >> 2
-}
-
-func (it hashTableItem) isChained() bool {
-	return it.next&chainedFlag != 0
-}
-
-func (it hashTableItem) isEmpty() bool {
-	return it.key.IsNil()
-}
-
-func (it *hashTableItem) setNext(next uintptr, flags uintptr) {
-	it.next = next<<2 | flags
-}
-
-func (it *hashTableItem) nextFlags() uintptr {
-	return it.next & nextFlags
-}
-
-const noNextFree uintptr = 1<<uintptrLen - 1
-
+// A hashTable contains an array of slots, `S_1, ... S_N` which contain
+// key-value pairs.  Each slot can also point at (at most) another slot, which
+// we represent as `S_i -> S_j`.
+//
+// By following the arrow we can form sequences of slots which we call "chains".
+//
+// There is a function that maps each key `k` to a given slot `S(k)` (in
+// practice we use hash(k) % N).  Let's call this slot the "primary slot" of
+// `k`.
+//
+// As we populate the hash table, we keep the following invariants:
+//
+// (I1) All chains are finite (no cycles).
+//
+// (I2) All items in a chain have the same primary slot.
+//
+// (I3) The first item in a chain is in its primary slot.
+//
+// Having those invariants mean that it is easy to find a key `k` in the table:
+// just start at its primary slot and follow the chain until you find a slot
+// containing `k` or reach the end of the chain.
+//
+// To insert a new key-value pair k => v into the table, consider the primary
+// slot `S` of `k`.  There are 3 possibilities.
+//
+// (1) Slot `S` is free.  This is simple: just put (k, v) in this slot.
+//
+// (2) Slot `S` already contains an item `J` for which it is the primary slot.
+// Move it to the next free slot `F` and put the new key-value pair in `S`, so
+// that the chain `S -> S'...` becomes `S -> F -> S'...`
+//
+// (3) Slot `S` contains an item `J` not in its primary position.  Because of
+// (I2) there is a chain `...S' -> S -> S''...`.  We move `J` to the next free
+// slot `F`, adjusting the chain as `...S' -> F -> S''...`.  That frees slot
+// `S`, which means we can put the new item in it.
+//
+// It is easy to check that in the 3 cases all invariants (I1), (I2) and (I3)
+// are preserved.
 type hashTable struct {
-	items    []hashTableItem
+	slots    []hashTableSlot
 	nextFree uintptr
 	base     uint8
 }
 
+type hashTableSlot struct {
+	key, value Value
+	next       uintptr // Where to look next for colliding keys (and flags)
+}
+
+const (
+	hasNextFlag uintptr = 1 // flags that another item is chained after this one
+	chainedFlag uintptr = 2 // flags that this item is chained (thus not in primary position)
+	nextFlags           = hasNextFlag | chainedFlag
+)
+
+const noNextFree uintptr = 1<<uintptrLen - 1
+
+// Small hash tables are treated differently (we bypass hashing the keys).
+const smallHashTableSize = 8
+
+func (it hashTableSlot) hasNext() bool {
+	return it.next&hasNextFlag != 0
+}
+
+func (it hashTableSlot) nextIndex() uintptr {
+	return it.next >> 2
+}
+
+func (it hashTableSlot) isChained() bool {
+	return it.next&chainedFlag != 0
+}
+
+func (it hashTableSlot) isEmpty() bool {
+	return it.key.IsNil()
+}
+
+func (it *hashTableSlot) setNext(next uintptr, flags uintptr) {
+	it.next = next<<2 | flags
+}
+
+func (it *hashTableSlot) nextFlags() uintptr {
+	return it.next & nextFlags
+}
+
 func (t *hashTable) set(k, v Value) {
-	if setKeyValue(t.items, (1<<t.base)-1, k, v, t.nextFree) {
-		t.nextFree = updateNextFree(t.items, t.nextFree)
+	if setKeyValue(t.slots, (1<<t.base)-1, k, v, t.nextFree) {
+		t.nextFree = updateNextFree(t.slots, t.nextFree)
 	}
 }
 
@@ -206,14 +283,14 @@ func (t *hashTable) reset(k, v Value) bool {
 	if t == nil {
 		return false
 	}
-	return resetKeyValue(t.items, (1<<t.base)-1, k, v)
+	return resetKeyValue(t.slots, (1<<t.base)-1, k, v)
 }
 
 func (t *hashTable) find(k Value) Value {
 	if t == nil {
 		return NilValue
 	}
-	it, _ := findItem(t.items, (1<<t.base)-1, k)
+	it, _ := findSlot(t.slots, (1<<t.base)-1, k)
 	if it == nil {
 		return NilValue
 	}
@@ -224,7 +301,7 @@ func (t *hashTable) removeKey(k Value) (wasSet bool) {
 	if t == nil {
 		return false
 	}
-	return removeKey(t.items, (1<<t.base)-1, k)
+	return removeKey(t.slots, (1<<t.base)-1, k)
 }
 
 func (t *hashTable) full() bool {
@@ -234,20 +311,20 @@ func (t *hashTable) full() bool {
 func (t *hashTable) grow() *hashTable {
 	if t == nil {
 		return &hashTable{
-			items: make([]hashTableItem, 1),
+			slots: make([]hashTableSlot, 1),
 		}
 	}
 	var (
 		base          = t.base + 1
 		sz    uintptr = 1 << base
 		mask  uintptr = sz - 1
-		items         = make([]hashTableItem, sz)
+		items         = make([]hashTableSlot, sz)
 	)
 
 	// Populate the new
-	t.nextFree = copyItems(items, t.items, mask, mask)
+	t.nextFree = copyItems(items, t.slots, mask, mask)
 	t.base = base
-	t.items = items
+	t.slots = items
 	return t
 }
 
@@ -255,10 +332,10 @@ func (t *hashTable) cleanup() {
 	if t == nil {
 		return
 	}
-	mask := uintptr(len(t.items) - 1)
-	items := make([]hashTableItem, len(t.items))
-	t.nextFree = copyItems(items, t.items, mask, mask)
-	t.items = items
+	mask := uintptr(len(t.slots) - 1)
+	items := make([]hashTableSlot, len(t.slots))
+	t.nextFree = copyItems(items, t.slots, mask, mask)
+	t.slots = items
 }
 
 func (t *hashTable) next(k Value) (next Value, v Value, ok bool) {
@@ -269,8 +346,8 @@ func (t *hashTable) next(k Value) (next Value, v Value, ok bool) {
 	// Find the starting point
 	var i uintptr
 	if !k.IsNil() {
-		var it *hashTableItem
-		it, i = findItem(t.items, (1<<t.base)-1, k)
+		var it *hashTableSlot
+		it, i = findSlot(t.slots, (1<<t.base)-1, k)
 		if it == nil {
 			return
 		}
@@ -278,12 +355,12 @@ func (t *hashTable) next(k Value) (next Value, v Value, ok bool) {
 	}
 
 	// Iterate to the next item
-	var nextIt hashTableItem
+	var nextIt hashTableSlot
 	for {
-		if int(i) >= len(t.items) {
+		if int(i) >= len(t.slots) {
 			return NilValue, NilValue, true
 		}
-		nextIt = t.items[i]
+		nextIt = t.slots[i]
 		if !nextIt.value.IsNil() {
 			return nextIt.key, nextIt.value, true
 		}
@@ -295,7 +372,7 @@ func (t *hashTable) classifyIndices(idxCountByLen *[uintptrLen]uintptr) (idxCoun
 	if t == nil {
 		return
 	}
-	for _, it := range t.items {
+	for _, it := range t.slots {
 		if it.value.IsNil() {
 			continue
 		}
@@ -306,7 +383,7 @@ func (t *hashTable) classifyIndices(idxCountByLen *[uintptrLen]uintptr) (idxCoun
 	}
 	return
 }
-func copyItems(items, from []hashTableItem, mask uintptr, nextFree uintptr) uintptr {
+func copyItems(items, from []hashTableSlot, mask uintptr, nextFree uintptr) uintptr {
 	for _, it := range from {
 		if !it.value.IsNil() {
 			if insertNewKeyValue(items, mask, it.key, it.value, nextFree) {
@@ -317,16 +394,16 @@ func copyItems(items, from []hashTableItem, mask uintptr, nextFree uintptr) uint
 	return nextFree
 }
 
-func setKeyValue(items []hashTableItem, mask uintptr, k, v Value, nextFree uintptr) bool {
-	if it, _ := findItem(items, mask, k); it != nil {
+func setKeyValue(items []hashTableSlot, mask uintptr, k, v Value, nextFree uintptr) bool {
+	if it, _ := findSlot(items, mask, k); it != nil {
 		it.value = v
 		return false
 	}
 	return insertNewKeyValue(items, mask, k, v, nextFree)
 }
 
-func resetKeyValue(items []hashTableItem, mask uintptr, k, v Value) (wasSet bool) {
-	it, _ := findItem(items, mask, k)
+func resetKeyValue(items []hashTableSlot, mask uintptr, k, v Value) (wasSet bool) {
+	it, _ := findSlot(items, mask, k)
 	wasSet = it != nil && !it.value.IsNil()
 	if wasSet {
 		it.value = v
@@ -334,8 +411,8 @@ func resetKeyValue(items []hashTableItem, mask uintptr, k, v Value) (wasSet bool
 	return
 }
 
-func insertNewKeyValue(items []hashTableItem, mask uintptr, k, v Value, nextFree uintptr) bool {
-	it := hashTableItem{key: k, value: v}
+func insertNewKeyValue(items []hashTableSlot, mask uintptr, k, v Value, nextFree uintptr) bool {
+	it := hashTableSlot{key: k, value: v}
 
 	// Just fill a small table, it's faster than calculating hashes.
 	if mask < smallHashTableSize {
@@ -365,7 +442,7 @@ func insertNewKeyValue(items []hashTableItem, mask uintptr, k, v Value, nextFree
 		return true
 	default:
 		// Colliding item is in primary position, put new item into free position.
-		cit.next |= 2
+		cit.next |= chainedFlag
 		items[nextFree] = cit
 		it.setNext(nextFree, hasNextFlag)
 		items[i] = it
@@ -373,18 +450,18 @@ func insertNewKeyValue(items []hashTableItem, mask uintptr, k, v Value, nextFree
 	}
 }
 
-func updateNextFree(items []hashTableItem, nextFree uintptr) uintptr {
-	for nextFree != noNextFree && !items[nextFree].isEmpty() {
+func updateNextFree(slots []hashTableSlot, nextFree uintptr) uintptr {
+	for nextFree != noNextFree && !slots[nextFree].isEmpty() {
 		nextFree--
 	}
 	return nextFree
 }
 
-func findItem(items []hashTableItem, mask uintptr, k Value) (it *hashTableItem, i uintptr) {
+func findSlot(slots []hashTableSlot, mask uintptr, k Value) (it *hashTableSlot, i uintptr) {
 	// For a small table, it's cheaper not to calculate the hash
 	if mask < smallHashTableSize {
 		for j := int(mask); j >= 0; j-- {
-			it = &items[j]
+			it = &slots[j]
 			if it.key.Equals(k) {
 				i = uintptr(j)
 				return
@@ -393,19 +470,22 @@ func findItem(items []hashTableItem, mask uintptr, k Value) (it *hashTableItem, 
 		return nil, 0
 	}
 	i = k.Hash() & mask
-	it = &items[i]
+	it = &slots[i]
+	if it.isChained() {
+		return nil, 0
+	}
 	for !it.key.Equals(k) {
 		if !it.hasNext() {
 			return nil, 0
 		}
 		i = it.nextIndex()
-		it = &items[i]
+		it = &slots[i]
 	}
 	return
 }
 
-func removeKey(items []hashTableItem, mask uintptr, k Value) (wasSet bool) {
-	if it, _ := findItem(items, mask, k); it != nil {
+func removeKey(slots []hashTableSlot, mask uintptr, k Value) (wasSet bool) {
+	if it, _ := findSlot(slots, mask, k); it != nil {
 		wasSet = !it.value.IsNil()
 		it.value = NilValue
 	}
