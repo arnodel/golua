28ec21efc12a38730ec6e506eea8467461f42abc
Arnaud Delobelle <arnodel@gmail.com>
2021-11-13 18:37:35 +0000

    Add quota.catch lib function and lua tests
    This will allow testing quota everywhere in lua tests

---
+++ lib/quotalib/lua_test.go
@@ -0,0 +1,16 @@
+package quotalib_test
+
+import (
+	"testing"
+
+	"github.com/arnodel/golua/lib"
+	"github.com/arnodel/golua/luatesting"
+	rt "github.com/arnodel/golua/runtime"
+)
+
+func TestQuotaLib(t *testing.T) {
+	luatesting.RunLuaTestsInDir(t, "lua", func(r *rt.Runtime) func() {
+		r.AllowQuotaModificationsInLua()
+		return lib.LoadAll(r)
+	})
+}
