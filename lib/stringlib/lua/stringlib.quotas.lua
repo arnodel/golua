e9d4ab93d7f5fcac1d64f9717b007091f3debcdf
Arnaud Delobelle <arnodel@gmail.com>
2021-11-15 12:03:28 +0000

    Some quotas tests for stringlib

---
+++ lib/stringlib/lua/stringlib.quotas.lua
@@ -0,0 +1,48 @@
+quota = require'quota'
+string = require 'string'
+
+s = "helLO"
+s1000 = s:rep(1000)
+
+-- string.lower consumes memory
+print(quota.rcall(0, 4000, string.lower, s))
+--> =true	hello
+
+print(quota.rcall(0, 4000, string.lower, s1000))
+--> =false
+
+
+-- string.upper consumes memory
+
+print(quota.rcall(0, 4000, string.upper, s))
+--> =true	HELLO
+
+print(quota.rcall(0, 4000, string.upper, s1000))
+--> =false
+
+
+-- string.reverse consumes memory
+
+print(quota.rcall(0, 4000, string.reverse, s))
+--> =true	OLleh
+
+print(quota.rcall(0, 4000, string.reverse, s1000))
+--> =false
+
+
+-- string.sub consumes memory
+
+print(quota.rcall(0, 1000, string.sub, s, 3, 2000))
+--> =true	lLO
+
+print(quota.rcall(0, 1000, string.sub, s1000, 3, 2000))
+--> =false
+
+
+-- string.char consumes memory
+
+print(quota.rcall(0, 1000, string.char, s:byte(1, #s)))
+--> =true	helLO
+
+print(quota.rcall(0, 1000, string.char, s1000:byte(1, 1200)))
+--> =false
