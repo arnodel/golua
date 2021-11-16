1ae078e9255a72414a62f3fc01b723a9c7a8c415
Arnaud Delobelle <arnodel@gmail.com>
2021-11-16 09:54:02 +0000

    Make builds with tag noquotas work again

--- runtime/quotamanager_noquotas.go
+++ runtime/quotamanager_noquotas.go
@@ -31,10 +31,23 @@ func (m *quotaManager) CPUQuotaStatus() (uint64, uint64) {
 func (m *quotaManager) RequireMem(memAmount uint64) {
 }
 
-func (m *quotaManager) releaseMem(memAmount uint64) {
+func (m *quotaManager) RequireSize(sz uintptr) {
+}
+
+func (m *quotaManager) RequireArrSize(sz uintptr, n int) {
+}
+
+func (m *quotaManager) RequireBytes(n int) {
+}
+
+func (m *quotaManager) ReleaseMem(memAmount uint64) {
 }
 
 func (m *quotaManager) UpdateMemQuota(newQuota uint64) {
+}
+
+func (m *quotaManager) UnusedMem() uint64 {
+	return 0
 }
 
 func (m *quotaManager) MemQuotaStatus() (uint64, uint64) {
