67a2568fc2513516538020546949882e5a7c50e2
Arnaud Delobelle <arnodel@gmail.com>
2021-11-29 18:03:38 +0000

    Lua tests for setupvalue

--- lib/debuglib/lua/upvalues.lua
+++ lib/debuglib/lua/upvalues.lua
@@ -1,7 +1,13 @@
+local function perr(...)
+    local ok, err = pcall(...)
+    if not ok then
+        print(err)
+    end
+end
 
 -- getupvalue tests
 do
-    function outer(x, y, z)
+    local function outer(x, y, z)
         return function()
             return x, y, z
         end
@@ -32,5 +38,37 @@ do
     --> =#1 must be a lua function
 
     perr(debug.getupvalue, inner, "a")
+    --> =#2 must be an integer
+end
+
+-- setupvalue tests
+do
+    local function outer(x, y, z)
+        return function()
+            return x, y, z
+        end
+    end
+
+    local inner = outer("hello", 1, false)
+
+    print(debug.setupvalue(inner, 1, "bye"))
+    --> =x
+    print(debug.setupvalue(inner, 2, 42))
+    --> =y
+    print(debug.setupvalue(inner, 3, true))
+    --> =z
+    print(debug.setupvalue(inner, 4, "non-existent"))
+    --> =
+
+    print(inner())
+    --> =bye	42	true
+
+    perr(debug.setupvalue, inner, 1)
+    --> =3 arguments needed
+
+    perr(debug.setupvalue, {}, 1, 2)
+    --> =#1 must be a lua function
+
+    perr(debug.setupvalue, inner, "x", 3)
     --> =#2 must be an integer
 end
