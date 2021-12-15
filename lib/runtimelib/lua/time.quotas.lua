18d2749caae376465ff7061508539145df94d7f1
Arnaud Delobelle <arnodel@gmail.com>
2021-12-15 16:36:38 +0000

    Relax constraint to make tests pass

--- lib/runtimelib/lua/time.quotas.lua
+++ lib/runtimelib/lua/time.quotas.lua
@@ -20,7 +20,7 @@ print(ctx.used.time >= 10)
 --> =true
 
 -- It didn't last much more than that (could be flaky)
-print(ctx.used.time <= 11)
+print(ctx.used.time <= 12)
 --> =true
 
 -- Significant work was done while it lasted (could be flaky)
