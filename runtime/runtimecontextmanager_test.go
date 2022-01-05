cf8226c4d0f68609802c9be335e82c349e7d5b86
Arnaud Delobelle <arnodel@gmail.com>
2022-01-05 08:33:42 +0000

    Tests for TerminatContext panic conditions

--- runtime/runtimecontextmanager_test.go
+++ runtime/runtimecontextmanager_test.go
@@ -3,7 +3,9 @@
 
 package runtime
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_runtimeContextManager_LinearUnused(t *testing.T) {
 	type fields struct {
@@ -66,6 +68,60 @@ func Test_runtimeContextManager_LinearUnused(t *testing.T) {
 			if got := m.LinearUnused(tt.args.cpuFactor); got != tt.want {
 				t.Errorf("runtimeContextManager.LinearUnused() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_runtimeContextManager_TerminateContext(t *testing.T) {
+	type fields struct {
+		status RuntimeContextStatus
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		wantPanic bool
+	}{
+		{
+			name:      "live context",
+			fields:    fields{status: StatusLive},
+			wantPanic: true,
+		},
+		{
+			name:      "done context",
+			fields:    fields{status: StatusDone},
+			wantPanic: false,
+		},
+		{
+			name:      "error context",
+			fields:    fields{status: StatusError},
+			wantPanic: false,
+		},
+		{
+			name:      "killed context",
+			fields:    fields{status: StatusKilled},
+			wantPanic: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &runtimeContextManager{
+				status: tt.fields.status,
+			}
+			defer func() {
+				switch recover() {
+				case nil:
+					if tt.wantPanic {
+						t.Error("should panic")
+					}
+				default:
+					if !tt.wantPanic {
+						t.Error("should not panic")
+					} else if m.status != StatusKilled {
+						t.Error("context status should be StatusKilled")
+					}
+				}
+			}()
+			m.TerminateContext("error")
 		})
 	}
 }
