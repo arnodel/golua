4ab8de5adf80aa75c3be3e2ba8e1e7f8e8bcf093
Arnaud Delobelle <arnodel@gmail.com>
2022-01-11 10:26:49 +0000

    Bitwise operations require numeric arguments
    (No string coercion)
    With Lua tests

--- runtime/lua/bitwise.lua
+++ runtime/lua/bitwise.lua
@@ -6,3 +6,32 @@ print(1 >> -3)
 
 print(10 << -2)
 --> =2
+
+local function perr(s)
+    local ok, err = pcall(load("return " .. s))
+    print(err)
+end
+
+perr[[100 & "2"]]
+--> ~.*attempt to perform bitwise and on a string value
+
+perr[["100" | 23]]
+--> ~.*attempt to perform bitwise or on a string value
+
+perr[[~"55"]]
+--> ~.*attempt to perform bitwise not on a string value
+
+perr[[23 ~ "5"]]
+--> ~.*attempt to perform bitwise xor on a string value
+
+perr[["77" >> 9]]
+--> ~.*attempt to perform bitwise shr on a string value
+
+perr[["123" << "456" ]]
+--> ~.*attempt to perform bitwise shl on a string value
+
+perr[[0.5 | 1.2]]
+--> ~.*number has no integer representation
+
+perr[[45.2 >> "a"]]
+--> ~.*attempt to perform bitwise shr on a string value
