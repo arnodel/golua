de17e4f4b254f98e66400ba69c626ab16542fddd
Arnaud Delobelle <arnodel@gmail.com>
2022-01-13 08:23:52 +0000

    Tests for non-wrapping integer literals (#50)
    In Lua 5.4 they no longer wrap but turn to floats instead.  Golua was already doing this!

--- runtime/lua/const.lua
+++ runtime/lua/const.lua
@@ -7,6 +7,15 @@ load(d)()
 print(load("x == y"))
 --> ~nil\t.*
 
+-- Hexadecimal literals are truncated
+print(0x12345678900000000000000000000ff)
+--> =255
+
+-- Decimal integer literals that do not fit into an integer are turned to
+-- floats (specified in Lua 5.4)
+print(5000000000000000000000000000000000000)
+--> =5e+36
+
 print(1e999999, -1e9999, 1e-99999)
 --> =+Inf	-Inf	0
 
