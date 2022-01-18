203406161754c89c100932bab872872a9b692298
Arnaud Delobelle <arnodel@gmail.com>
2022-01-18 08:28:15 +0000

    Fix Thread.IsMain()

--- runtime/thread.go
+++ runtime/thread.go
@@ -55,7 +55,7 @@ func (t *Thread) CurrentCont() Cont {
 
 // IsMain returns true if the thread is the runtime's main thread.
 func (t *Thread) IsMain() bool {
-	return t.caller == nil
+	return t == t.mainThread
 }
 
 const maxErrorsInMessageHandler = 10
