0f236721fd3d17c3243ddc906b970dc28f3dd07d
Arnaud Delobelle <arnodel@gmail.com>
2022-01-19 16:21:31 +0000

    Fix test string

--- runtime/lua/errors.lua
+++ runtime/lua/errors.lua
@@ -2,4 +2,4 @@ local a
  for i=1,'a' do 
  print(i) 
 end
---> =!!! runtime: error: luatest:6: 'for' limit: expected number, got string
+--> ~!!! runtime: error: luatest:\d+: 'for' limit: expected number, got string
