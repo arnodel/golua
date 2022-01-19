3a9bc69563a2ca4dc31f5cff800a8f8c00e8ec6c
Arnaud Delobelle <arnodel@gmail.com>
2022-01-19 09:20:48 +0000

    Improve runtime coverage

--- runtime/lua/errors.lua
+++ runtime/lua/errors.lua
@@ -1,5 +1,10 @@
+-- error in message handler, we eventually bail out
+print(xpcall(error, error, "foo"))
+--> =false	error in error handling
+
 local a
  for i=1,'a' do 
  print(i) 
 end
---> =!!! runtime: error: luatest:2: expected numeric value
+--> =!!! runtime: error: luatest:6: expected numeric value
+
