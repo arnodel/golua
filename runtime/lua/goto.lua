e1bcf7188a58fed2e2d9fbdae286d008469d3715
Arnaud Delobelle <arnodel@gmail.com>
2022-01-20 08:44:14 +0000

    Lua 5.4: Error when declaring a label with an existing name (#53)
    * Error when declaring a label with an existing name
    
    Only in the current function's lexical scope
    
    * Point at line where existing label is defined in error message

--- runtime/lua/goto.lua
+++ runtime/lua/goto.lua
@@ -17,4 +17,40 @@ print(load([[
     ::cont::
   until xuxu < x
 ]]))
---> ~nil\t.*undefined label 'cont'
+--> ~nil\t.*no visible label 'cont'
+
+-- Lua 5.4 forbids shadowing labels
+
+print(load[[
+  ::label::
+  print"hello"
+  ::label::
+  print"bye"
+]])
+--> ~nil\t.*label 'label' already defined at line 1
+
+print(load[[
+  ::foo::
+  do
+    ::foo::
+    goto foo
+  end
+]])
+--> ~nil\t.*label 'foo' already defined at line 1
+
+-- It's OK to reuse a label name that was defined outside the current function
+-- scope.
+do
+  ::l2::
+  local f = function(n)
+    ::l2::
+    print(n)
+    n = n - 1
+    if n > 0 then
+      goto l2
+    end
+  end
+  f(2)
+end
+--> =2
+--> =1
