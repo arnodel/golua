61e780a4359d8d86a11c84aadf9c84d2c5f0f96b
Arnaud Delobelle <arnodel@gmail.com>
2022-02-01 16:35:04 +0000

    More lines covered this way

--- runtime/lib_test.go
+++ runtime/lib_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/arnodel/golua/scanner"
 )
 
-func TestRuntime_CompileLuaChunkOrExp(t *testing.T) {
+func TestRuntime_CompileAndLoadLuaChunkOrExp(t *testing.T) {
 	r := New(os.Stdout)
 	type args struct {
 		name           string
@@ -64,7 +64,7 @@ func TestRuntime_CompileLuaChunkOrExp(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, _, err := r.CompileLuaChunkOrExp(tt.args.name, tt.args.source, tt.args.scannerOptions...)
+			_, err := r.CompileAndLoadLuaChunkOrExp(tt.args.name, tt.args.source, NilValue, tt.args.scannerOptions...)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Runtime.CompileLuaChunkOrExp() error = %v, wantErr %v", err, tt.wantErr)
 				return
