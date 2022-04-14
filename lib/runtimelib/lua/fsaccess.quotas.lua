318ec72c838601cae95fc4005727a16628e7459e
Arnaud Delobelle <arnodel@gmail.com>
2022-04-14 21:11:33 +0100

    A bit of documentation

--- lib/runtimelib/lua/fsaccess.quotas.lua
+++ lib/runtimelib/lua/fsaccess.quotas.lua
@@ -1,3 +1,14 @@
+-- Tests for fs access.  The 'fs' field in the context is a list of rule, each rule of the form
+-- { prefix='prefix/path', allowed=ACTION_STRING, denied=ACTION_STRING }
+-- An ACTION_STRING is a string made of the following characters:
+-- - '*': all actions
+-- - 'r': read a file
+-- - 'w': write a file
+-- - 'c': create a file
+-- - 'd': delete a file
+-- - 'C': create any file in a directory (weird, used for creating temp files)
+
+-- Deny all operations in lua/testfiles/
 local ctx, err = runtime.callcontext({fs={{prefix="testfiles/", denied="*"}}}, function()
     -- This operation is not allowed so an error is thrown
     io.open("testfiles/existing-file.txt")
@@ -5,6 +16,7 @@ end)
 print(err)
 --> ~.*safeio: operation not allowed
 
+-- Allow only reading files in lua/testfiles/
 local ctx, err = runtime.callcontext({fs={{prefix="lua/testfiles/", allowed="r"}}}, function() 
     -- This operation is allowed but the file doesn't exist, so open returns an error
     local f <close>, err = io.open("lua/testfiles/non-existent-file")
@@ -17,6 +29,7 @@ end)
 print(err)
 --> ~.*safeio: operation not allowed
 
+-- Allow all operations apart from creating files in lua/testfiles/
 local ctx, err = runtime.callcontext({fs={{prefix="lua/testfiles/", allowed="*", denied="c"}}}, function() 
     -- This is allowed because the file exists
     local f <close>, err = io.open("lua/testfiles/existing-file.txt", "w")
@@ -29,6 +42,7 @@ end)
 print(err)
 --> ~.*safeio: operation not allowed
 
+-- Allow all operations in lua/testfiles/
 local ctx, err = runtime.callcontext({fs={{prefix="lua/testfiles/", allowed="*"}}}, function() 
     -- This operation succeeds because file creation is allowed
     local f <close>, err = io.open("lua/testfiles/non-existent-file.txt", "w")
