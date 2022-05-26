e623caadc5ca2aa5f9e60b581f5169c26754e606
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-05-26 12:53:04 -0400

    test: add io.popen tests

--- lib/iolib/lua/iolib.lua
+++ lib/iolib/lua/iolib.lua
@@ -81,6 +81,72 @@ do
 end
 
 do
+    print(pcall(io.popen))
+    --> ~^false\t.*value needed
+
+    print(pcall(io.popen, {}))
+    --> ~^false\t.*must be a string
+
+    print(pcall(io.popen, "aaa", false))
+    --> ~^false\t.*must be a string
+
+    testf(io.popen)("eejkgfdbnjb")
+    --> ~OK\tnil\t
+
+    local f = io.popen("cat files/iotest.txt")
+    print(f)
+    --> =file ("cat files/iotext.txt")
+
+    print(pcall(f.read))
+    --> ~^false\t.*value needed
+
+    print(pcall(f.read, 123))
+    --> ~^false\t
+
+    print(pcall(f.read, f, "?"))
+    --> ~^false\t.*invalid format
+
+    testf(io.type)()
+    --> ~ERR	.*value needed
+
+    print(io.type(123))
+    --> =nil
+
+    print(io.type(f))
+    --> =file
+
+    print(pcall(f.lines))
+    --> ~^false\t.*value needed
+
+    print(pcall(f.lines, 123))
+    --> ~^false\t.*must be a file
+
+    print(pcall(f.lines, f, "wat"))
+    --> ~^false\t.*invalid format
+
+
+    for line in f:lines() do
+        print(line)
+    end
+    --> =hello
+    --> =123
+    --> =bye
+
+    testf(f.close)()
+    --> ~ERR	.*value needed
+
+    testf(f.flush)()
+    --> ~ERR	.*value needed
+
+    testf(f.flush)(123)
+    --> ~ERR	.*must be a file
+
+    f:close()
+    print(io.type(f))
+    --> =closed file
+end
+
+do
     testf(io.lines)(123)
     --> ~ERR	.*must be a string
 
@@ -171,6 +237,34 @@ do
 
     testf(metaf.__tostring)("not a file")
     --> ~ERR	.*must be a file
+end
+
+do
+    local function wp(x)
+        print("[" .. tostring(x) .. "]")
+    end
+    local f = io.popen("cat > files/popenwrite.txt", "w")
+
+    testf(f.write)()
+    --> ~ERR	.*value needed
+
+    print(pcall(f.write, 123))
+    --> ~^false\t.*must be a file
+
+    print(pcall(f.write, f, {}))
+    --> ~^false\t.*must be a string or a number
+
+    f:write("foobar", 1234, "\nabc\n")
+    f:close()
+    f = io.open("files/popenwrite.txt", "r")
+
+    wp(f:read("a"))
+    --> =[foobar1234
+    --> =abc
+    --> =]
+
+    wp(f:read("l"))
+    --> =[nil]
 end
 
 do
